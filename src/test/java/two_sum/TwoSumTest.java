ba30e64f296c0b6241f496099b7decd28462a7a5
ranjitshinde <ranjitshinde91@gmail.com>
2021-12-28 10:33:29 +0530

    adds TwoSum.java

---
+++ src/test/java/two_sum/TwoSumTest.java
@@ -0,0 +1,38 @@
+package two_sum;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TwoSumTest {
+
+    @Test
+    void test1() {
+        var solution = new Solution();
+
+        int[] ints = solution.twoSum(new int[]{2, 7, 11, 15}, 9);
+
+        assertThat(ints[0]).isEqualTo(0);
+        assertThat(ints[1]).isEqualTo(1);
+    }
+
+    @Test
+    void test2() {
+        var solution = new Solution();
+
+        int[] ints = solution.twoSum(new int[]{3, 2, 4}, 6);
+
+        assertThat(ints[0]).isEqualTo(1);
+        assertThat(ints[1]).isEqualTo(2);
+    }
+
+    @Test
+    void test3() {
+        var solution = new Solution();
+
+        int[] ints = solution.twoSum(new int[]{3, 3}, 6);
+
+        assertThat(ints[0]).isEqualTo(0);
+        assertThat(ints[1]).isEqualTo(1);
+    }
+}
