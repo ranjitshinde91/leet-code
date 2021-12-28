226d0437535f18b6198192f42991b7d2350d3f10
ranjitshinde <ranjitshinde91@gmail.com>
2021-12-28 19:37:44 +0530

    adds CombinationSumIV

---
+++ src/main/java/combination_sum_4/CombinationSumIV.java
@@ -0,0 +1,39 @@
+package combination_sum_4;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    private Map<String, Integer> cache;
+
+    public Solution() {
+        this.cache = new HashMap<>();
+    }
+
+    public int combinationSum4(int[] nums, int target) {
+        if (target < 0) {
+            return 0;
+        }
+        if (target == 0) {
+            return 1;
+        }
+        String key = key(nums, target);
+        if (this.cache.containsKey(key)) {
+            return this.cache.get(key);
+        }
+        int total = 0;
+        for (int num : nums) {
+            total += combinationSum4(nums, target - num);
+        }
+        this.cache.put(key, total);
+        return total;
+    }
+
+    private String key(int[] nums, int target) {
+        StringBuilder key = new StringBuilder(String.valueOf(target));
+        for (int num : nums) {
+            key.append(":").append(num);
+        }
+        return key.toString();
+    }
+}
---
+++ src/test/java/combination_sum_4/CombinationSumIVTest.java
@@ -0,0 +1,33 @@
+package combination_sum_4;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CombinationSumIVTest {
+
+
+    @ParameterizedTest
+    @MethodSource("inputs")
+    void test(int[] input, int target, int expected) {
+        var solution = new Solution();
+
+        int numberOfPossibleCombinations = solution.combinationSum4(input, target);
+
+        assertThat(numberOfPossibleCombinations).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> inputs() {
+        return Stream.of(
+                Arguments.of(new int[]{9}, 3, 0),
+                Arguments.of(new int[]{3, 4}, 2, 0),
+                Arguments.of(new int[]{1, 2}, 3, 3),
+                Arguments.of(new int[]{1, 2, 3}, 4, 7),
+                Arguments.of(new int[]{2, 1, 3}, 35, 1132436852)
+        );
+    }
+}
