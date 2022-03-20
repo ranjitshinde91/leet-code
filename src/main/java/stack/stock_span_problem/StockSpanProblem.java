981bc5470d085cfe7c37f11680b2a91ea9b5a074
ranjitshindetwks <98303304+ranjitshindetwks@users.noreply.github.com>
2022-03-20 10:35:37 +0530

    updates StockSpanProblem.java

--- src/main/java/stack/stock_span_problem/StockSpanProblem.java
+++ src/main/java/stack/stock_span_problem/StockSpanProblem.java
@@ -3,15 +3,17 @@ package stack.stock_span_problem;
 import java.util.Stack;
 
 class Solution {
-    //Function to calculate the span of stockâ€™s price for all n days.
+    //Function to calculate the span of stock price for all n days.
     public static int[] calculateSpan1(int price[], int n) {
         int[] ints = new int[n];
         ints[0] = 1;
 
         for (int i = 1; i < price.length; i++) {
             int span = 1;
-            for (int k = i - 1; k >= 0 && price[k] < price[i]; k--) {
+            int k = i - 1;
+            while (k >= 0 && price[k] <= price[i]) {
                 span++;
+                k--;
             }
             ints[i] = span;
         }
