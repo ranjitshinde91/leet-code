957848b9b9911b20b9bb67fc5a5cacd4e6603fc6
ranjitshindetwks <98303304+ranjitshindetwks@users.noreply.github.com>
2022-06-04 15:07:16 +0530

    adds MinHeap.java

---
+++ src/main/java/heap/min_heap/MinHeap.java
@@ -0,0 +1,112 @@
+package heap.min_heap;
+
+
+public class MinHeap {
+
+    private final int capacity;
+    private final int[] array;
+    private int size = 0;
+
+
+    MinHeap(int capacity) {
+        this.capacity = capacity;
+        this.array = new int[capacity];
+    }
+
+    public MinHeap(int[] ints) {
+        this.capacity = ints.length;
+        this.array = ints;
+        this.size = ints.length;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public int left(int i) {
+        return 2 * i + 1;
+    }
+
+    public int right(int i) {
+        return 2 * i + 2;
+    }
+
+    public int parent(int i) {
+        return (i - 1) / 2;
+    }
+
+    public int insert(int element) {
+        array[size] = element;
+        rearrange(size);
+        size++;
+        return size;
+    }
+
+    public void rearrange(final int i) {
+        int index = i;
+        while (index > 0 && array[parent(index)] > array[index]) {
+            int temp = array[index];
+            array[index] = array[parent(index)];
+            array[parent(index)] = temp;
+            index = parent(index);
+        }
+    }
+
+    public int[] values() {
+        var temp = new int[size];
+        for (int i = 0; i < size; i++) {
+            temp[i] = array[i];
+        }
+        return temp;
+    }
+
+    public void print() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public void heapify(int index) {
+
+        int left = left(index);
+        int right = right(index);
+
+        int smallest = index;
+        if (left < size && array[left] < array[index]) {
+            smallest = left;
+        }
+        if (right < size && array[right] < array[smallest]) {
+            smallest = right;
+        }
+        if (smallest != index) {
+            int temp = array[smallest];
+            array[smallest] = array[index];
+            array[index] = temp;
+
+            heapify(smallest);
+        }
+    }
+
+    public int getMin(){
+        if(size()>0){
+            return array[0];
+        }
+        return -1;
+    }
+
+    public int deleteMin(){
+        if(size == 0){
+            return -1;
+        }
+        if(size ==1){
+            size--;
+            return array[0];
+        }
+        int min = array[0];
+        array[0] = array[size-1];
+        size--;
+        heapify(0);
+        return min;
+    }
+}
---
+++ src/test/java/heap/min_heap/MinHeapTest.java
@@ -0,0 +1,43 @@
+package heap.min_heap;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MinHeapTest {
+
+    @Test
+    void shouldInsertElementInMinHeap() {
+        MinHeap minHeap = new MinHeap(10);
+
+        minHeap.insert(10);
+        minHeap.insert(5);
+        minHeap.insert(20);
+        minHeap.insert(30);
+        minHeap.insert(50);
+
+        int[] values = minHeap.values();
+
+        assertThat(values).containsExactly(5, 10, 20, 30, 50);
+    }
+
+    @Test
+    void shouldPerformHeapify() {
+        MinHeap minHeap = new MinHeap(new int[]{40, 20, 30, 35, 25, 80, 32, 100, 70, 60});
+
+        minHeap.heapify(0);
+
+        var values = minHeap.values();
+        assertThat(values).containsExactly(20, 25, 30, 35, 40, 80, 32, 100, 70, 60);
+    }
+
+    @Test
+    void shouldDeleteMinElement() {
+        MinHeap minHeap = new MinHeap(new int[]{20, 25, 30, 35, 40, 80, 32, 100, 70, 60});
+
+        var minValue = minHeap.deleteMin();
+
+        assertThat(minValue).isEqualTo(20);
+        assertThat(minHeap.values()).containsExactly(25, 35,30, 60, 40, 80, 32, 100, 70);
+    }
+}
