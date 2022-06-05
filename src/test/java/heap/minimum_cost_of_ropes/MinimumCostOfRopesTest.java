746cdeb03ed151c502cd34580cd88636d869bb52
ranjitshindetwks <98303304+ranjitshindetwks@users.noreply.github.com>
2022-06-05 13:12:18 +0530

    adds tests for MinimumCostOfRopesTest.java

--- src/test/java/heap/minimum_cost_of_ropes/MinimumCostOfRopesTest.java
+++ src/test/java/heap/minimum_cost_of_ropes/MinimumCostOfRopesTest.java
@@ -1,7 +1,30 @@
 package heap.minimum_cost_of_ropes;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MinimumCostOfRopesTest {
+
+    @ParameterizedTest
+    @MethodSource("inputs")
+    void test1(long[] input, long expected) {
+        MinimumCostOfRopes minimumCostOfRopes = new MinimumCostOfRopes();
+
+        long cost = minimumCostOfRopes.minCost(input, input.length);
+
+        assertThat(cost).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> inputs() {
+        return Stream.of(
+                Arguments.of(new long[]{4, 3, 2, 6}, 29),
+                Arguments.of(new long[]{4, 2, 7, 6, 9}, 62)
+        );
+    }
 
 }
