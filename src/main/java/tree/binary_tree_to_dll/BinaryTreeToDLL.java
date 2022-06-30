1678e32cc78d0cd70ccad0fb74d5f9b4a0702b3e
ranjitshindetwks <98303304+ranjitshindetwks@users.noreply.github.com>
2022-06-30 11:26:04 +0530

    adds BinaryTreeToDLL.java

---
+++ src/main/java/tree/binary_tree_to_dll/BinaryTreeToDLL.java
@@ -0,0 +1,38 @@
+package tree.binary_tree_to_dll;
+
+import tree.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryTreeToDLL {
+
+    TreeNode bToDLL(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        inOrderTraversal(queue, root);
+        TreeNode head = queue.poll();
+        TreeNode lastNode = head;
+
+        while (!queue.isEmpty()) {
+            TreeNode temp = queue.poll();
+
+            lastNode.right = temp;
+            temp.left = lastNode;
+
+            lastNode = temp;
+        }
+        return head;
+    }
+
+    private void inOrderTraversal(Queue<TreeNode> queue, TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        inOrderTraversal(queue, node.left);
+        queue.add(node);
+        inOrderTraversal(queue, node.right);
+    }
+}
---
+++ src/test/java/tree/binary_tree_to_dll/BinaryTreeToDLLTest.java
@@ -0,0 +1,40 @@
+package tree.binary_tree_to_dll;
+
+import org.junit.jupiter.api.Test;
+import tree.TreeNode;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BinaryTreeToDLLTest {
+
+    @Test
+    void test() {
+        var three = new TreeNode(3);
+        var nine = new TreeNode(9);
+        var twenty = new TreeNode(20);
+        var fifteen = new TreeNode(15);
+        var seven = new TreeNode(7);
+
+        twenty.left = fifteen;
+        twenty.right = seven;
+
+        three.left = nine;
+        three.right = twenty;
+
+        var binaryTreeToDLL = new BinaryTreeToDLL();
+
+        var root = binaryTreeToDLL.bToDLL(three);
+
+        assertThat(root.val).isEqualTo(9);
+        assertThat(root.left).isNull();
+        assertThat(root.right).isEqualTo(three);
+
+        assertThat(root.right.val).isEqualTo(3);
+        assertThat(root.right.left).isEqualTo(nine);
+        assertThat(root.right.right).isEqualTo(fifteen);
+
+        assertThat(root.right.right.val).isEqualTo(15);
+        assertThat(root.right.right.left).isEqualTo(three);
+        assertThat(root.right.right.right).isEqualTo(twenty);
+    }
+}
