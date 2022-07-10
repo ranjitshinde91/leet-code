9768e7af728e119dfe7a4b23dbb70200da839801
ranjitshindetwks <98303304+ranjitshindetwks@users.noreply.github.com>
2022-07-10 21:36:43 +0530

    adds VerticalSumInABST.java

---
+++ src/main/java/binary_search_tree/vertical_sum_in_a_bst/VerticalSumInABST.java
@@ -0,0 +1,26 @@
+package binary_search_tree.vertical_sum_in_a_bst;
+
+import tree.Node;
+
+import java.util.*;
+
+public class VerticalSumInABST {
+
+    public List<Integer> sum(Node node) {
+        Map<Integer, Integer> map = new TreeMap<>();
+        _sum(node, 0, map);
+        return new ArrayList<>(map.values());
+    }
+
+    private void _sum(Node node, int level, Map<Integer, Integer> map) {
+        if (node != null) {
+            _sum(node.left, level - 1, map);
+            if (!map.containsKey(level)) {
+                map.computeIfAbsent(level, key -> node.data);
+            } else {
+                map.computeIfPresent(level, (key, value) -> value + node.data);
+            }
+            _sum(node.right, level + 1, map);
+        }
+    }
+}
---
+++ src/test/java/binary_search_tree/vertical_sum_in_a_bst/VerticalSumInABSTTest.java
@@ -0,0 +1,59 @@
+package binary_search_tree.vertical_sum_in_a_bst;
+
+import org.junit.jupiter.api.Test;
+import tree.Node;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class VerticalSumInABSTTest {
+
+    @Test
+    void test() {
+        var ten = new Node(10);
+        var twenty = new Node(20);
+        var thirty = new Node(30);
+        var five = new Node(5);
+        var fifteen = new Node(15);
+
+        ten.left = twenty;
+        ten.right = thirty;
+
+        twenty.left = five;
+        twenty.right = fifteen;
+
+        var verticalSumInABST = new VerticalSumInABST();
+
+        var traversal = verticalSumInABST.sum(ten);
+
+        assertThat(traversal).containsExactly(5, 20, 25, 30);
+    }
+
+    @Test
+    void test2() {
+        var ten = new Node(10);
+        var fifteen = new Node(15);
+        var twentyFive = new Node(25);
+        var thirtyFive = new Node(35);
+        var twenty = new Node(20);
+        var seventyFive = new Node(75);
+        var eighty = new Node(80);
+        var forty = new Node(40);
+
+        ten.left = fifteen;
+        ten.right = twentyFive;
+
+        fifteen.left = thirtyFive;
+        fifteen.right = twenty;
+
+        thirtyFive.left = forty;
+
+        twenty.right = seventyFive;
+        seventyFive.right = eighty;
+
+        var verticalSumInABST = new VerticalSumInABST();
+
+        var traversal = verticalSumInABST.sum(ten);
+
+        assertThat(traversal).containsExactly(40, 35, 15, 30, 100, 80);
+    }
+}
