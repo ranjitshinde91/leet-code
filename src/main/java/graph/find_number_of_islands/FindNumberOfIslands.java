168e9d69dd5b9f71fc8aba8cf9a59d05f43b0cce
ranjitshindetwks <98303304+ranjitshindetwks@users.noreply.github.com>
2022-07-12 11:48:16 +0530

    adds FindNumberOfIslands.java

---
+++ src/main/java/graph/find_number_of_islands/FindNumberOfIslands.java
@@ -0,0 +1,47 @@
+package graph.find_number_of_islands;
+
+public class FindNumberOfIslands {
+
+    public int numIslands(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        boolean[][] visited = new boolean[n][m];
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1' && visited[i][j] == false) {
+                    visit(grid, i, j, n, m, visited);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void visit(char[][] grid, int i, int j, int n, int m, boolean[][] visited) {
+        if (i >= n || j >= m || i < 0 || j < 0) {
+            return;
+        }
+        if (grid[i][j] == '0') {
+            return;
+        }
+        if (visited[i][j]) {
+            return;
+        }
+
+        visited[i][j] = true;
+
+        visit(grid, i, j + 1, n, m, visited);
+        visit(grid, i, j - 1, n, m, visited);
+
+        visit(grid, i - 1, j, n, m, visited);
+        visit(grid, i + 1, j, n, m, visited);
+
+        visit(grid, i - 1, j + 1, n, m, visited);
+        visit(grid, i + 1, j + 1, n, m, visited);
+
+        visit(grid, i - 1, j - 1, n, m, visited);
+        visit(grid, i + 1, j - 1, n, m, visited);
+    }
+}
---
+++ src/test/java/graph/find_number_of_islands/FindNumberOfIslandsTest.java
@@ -0,0 +1,39 @@
+package graph.find_number_of_islands;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FindNumberOfIslandsTest {
+
+    @ParameterizedTest
+    @MethodSource("inputs")
+    void tabulation(char[][] grid, int expected) {
+        var findNumberOfIslands = new FindNumberOfIslands();
+
+        long output = findNumberOfIslands.numIslands(grid);
+
+        assertThat(output).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> inputs() {
+        return Stream.of(
+                Arguments.of(
+                        new char[][]{
+                                {'0', '1'},
+                                {'1', '0'},
+                                {'1', '1'},
+                                {'1', '0'}
+                        }, 1),
+                Arguments.of(
+                        new char[][]{
+                                {'0', '1', '1', '1', '0', '0', '0'},
+                                {'0', '0', '1', '1', '0', '1', '0'}
+                        }, 2)
+        );
+    }
+}
