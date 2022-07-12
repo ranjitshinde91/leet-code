2b860a10a1c4863c7a990453ae343454595b4e19
ranjitshindetwks <98303304+ranjitshindetwks@users.noreply.github.com>
2022-07-12 22:53:45 +0530

    refactors TopologicalSort.java to remove visitor array bas there will be no cycle

--- src/main/java/graph/topological_sort/TopologicalSort.java
+++ src/main/java/graph/topological_sort/TopologicalSort.java
@@ -8,26 +8,22 @@ public class TopologicalSort {
         int[] sorted = new int[v];
         int index = 0;
         Queue<Integer> queue = new ArrayDeque<>();
-        boolean[] visited = new boolean[v];
 
         int[] inDegrees = inDegrees(adj, v);
         for (int i = 0; i < v; i++) {
-            if (!visited[i] && inDegrees[i] == 0) {
+            if (inDegrees[i] == 0) {
                 queue.add(i);
             }
         }
 
         while (!queue.isEmpty()) {
             Integer vertex = queue.poll();
-            visited[vertex] = true;
             sorted[index++] = vertex;
 
             for (int edge : adj.get(vertex)) {
-                if (!visited[edge]) {
-                    inDegrees[edge] = inDegrees[edge] - 1;
-                    if (inDegrees[edge] == 0) {
-                        queue.add(edge);
-                    }
+                inDegrees[edge] = inDegrees[edge] - 1;
+                if (inDegrees[edge] == 0) {
+                    queue.add(edge);
                 }
             }
         }
