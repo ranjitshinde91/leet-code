f90d5bb5222c1af79840d126ab2f2a22aa5cf6b2
ranjitshindetwks <98303304+ranjitshindetwks@users.noreply.github.com>
2022-07-12 19:27:08 +0530

    adds ShortestPathInAnUndirectedGraph.java

---
+++ src/main/java/graph/shortest_path_in_an_undirected_graph/ShortestPathInAnUndirectedGraph.java
@@ -0,0 +1,31 @@
+package graph.shortest_path_in_an_undirected_graph;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+
+public class ShortestPathInAnUndirectedGraph {
+
+    public int[] shortestPath(ArrayList<ArrayList<Integer>> adjacencyList, int source) {
+        int v = adjacencyList.size();
+        boolean[] visited = new boolean[v];
+        int[] distance = new int[v];
+        distance[0] = 0;
+        visited[0] = true;
+
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(source);
+
+        while (!queue.isEmpty()) {
+            Integer poll = queue.poll();
+            for (int i : adjacencyList.get(poll)) {
+                if (!visited[i]) {
+                    distance[i] = distance[poll] + 1;
+                    visited[i] = true;
+                    queue.add(i);
+                }
+            }
+        }
+        return distance;
+    }
+}
---
+++ src/test/java/graph/shortest_path_in_an_undirected_graph/ShortestPathInAnUndirectedGraphTest.java
@@ -0,0 +1,58 @@
+package graph.shortest_path_in_an_undirected_graph;
+
+import graph.GraphAdjacencyMatrixBuilder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+import static graph.GraphType.UNDIRECTED;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ShortestPathInAnUndirectedGraphTest {
+
+    @ParameterizedTest
+    @MethodSource("inputs")
+    void name(String input, int[] expected) {
+        ArrayList<ArrayList<Integer>> adjacencyList = GraphAdjacencyMatrixBuilder.build(input, UNDIRECTED);
+        var shortestPathInAnUndirectedGraph = new ShortestPathInAnUndirectedGraph();
+
+        int[] paths = shortestPathInAnUndirectedGraph.shortestPath(adjacencyList, 0);
+
+        assertThat(paths).containsExactly(expected);
+    }
+
+    private static Stream<Arguments> inputs() {
+        return Stream.of(
+                Arguments.of(
+                        "4 5\n" +
+                                "0 1\n" +
+                                "0 2\n" +
+                                "1 2\n" +
+                                "2 3\n" +
+                                "1 3"
+                        , new int[]{0, 1, 1, 2}),
+                Arguments.of(
+                        "4 4\n" +
+                                "0 1\n" +
+                                "2 3\n" +
+                                "0 2\n" +
+                                "0 3"
+                        , new int[]{0, 1, 1, 1}),
+                Arguments.of(
+                        "6 8\n" +
+                                "0 1\n" +
+                                "0 2\n" +
+                                "0 4\n" +
+                                "2 3\n" +
+                                "2 4\n" +
+                                "4 5\n" +
+                                "3 5\n" +
+                                "1 3"
+                        , new int[]{0, 1, 1, 2, 1, 2})
+        );
+    }
+
+}
