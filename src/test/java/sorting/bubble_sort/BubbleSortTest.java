0284a3cf53de97ada9feabe5a42b7ef94101a3c7
ranjitshindetwks <98303304+ranjitshindetwks@users.noreply.github.com>
2022-07-19 14:51:29 +0530

    adds bubble sort

---
+++ src/test/java/sorting/bubble_sort/BubbleSortTest.java
@@ -0,0 +1,39 @@
+package sorting.bubble_sort;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BubbleSortTest {
+
+    @ParameterizedTest
+    @MethodSource("inputs")
+    public void test(int[] input, int[] expected) {
+        var bubbleSort = new BubbleSort();
+
+        bubbleSort.bubbleSort(input, input.length);
+
+        assertThat(input).containsExactly(expected);
+    }
+
+    public static Stream<Arguments> inputs() {
+        return Stream.of(
+                Arguments.of(
+                        new int[]{4, 1, 3, 9, 7},
+                        new int[]{1, 3, 4, 7, 9}
+                ),
+                Arguments.of(
+                        new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
+                        new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+                ),
+                Arguments.of(
+                        new int[]{1, 2, 3, 4, 5},
+                        new int[]{1, 2, 3, 4, 5}
+                )
+        );
+    }
+}
