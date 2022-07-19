ed431f23a86661654ef070ce5bc1c52573965d2f
ranjitshindetwks <98303304+ranjitshindetwks@users.noreply.github.com>
2022-07-19 19:22:06 +0530

    adds insertion sort

---
+++ src/main/java/sorting/insertion_sort/InsertionSort.java
@@ -0,0 +1,16 @@
+package sorting.insertion_sort;
+
+public class InsertionSort {
+
+    public void insertionSort(int[] input, int n) {
+        for (int i = 1; i < n; i++) {
+            int temp = input[i];
+            int j = i - 1;
+            while (j >= 0 && input[j] > temp) {
+                input[j + 1] = input[j];
+                j--;
+            }
+            input[j + 1] = temp;
+        }
+    }
+}
