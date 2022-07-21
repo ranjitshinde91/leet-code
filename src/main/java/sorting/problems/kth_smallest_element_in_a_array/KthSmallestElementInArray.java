ca6c4461ae1d792614e79701371b2dfd02d4b131
ranjitshindetwks <98303304+ranjitshindetwks@users.noreply.github.com>
2022-07-21 19:58:53 +0530

    adds KthSmallestElementInArray.java

---
+++ src/main/java/sorting/problems/kth_smallest_element_in_a_array/KthSmallestElementInArray.java
@@ -0,0 +1,26 @@
+package sorting.problems.kth_smallest_element_in_a_array;
+
+import sorting.algorithms.quick_sort.partitions.LomutoPartition;
+
+public class KthSmallestElementInArray {
+
+
+    public int kthSmallest(int[] input, int n, int k) {
+        int low = 0;
+        int high = n - 1;
+        LomutoPartition lomutoPartition = new LomutoPartition();
+
+        while (low <= high) {
+            int partition = lomutoPartition.partition(input, low, high);
+            System.out.println("low, high partition, k-1 " + low + "," + high + "," + partition + " k-1 " + (k - 1));
+            if (k - 1 == partition) {
+                return input[partition];
+            } else if (partition > k - 1) {
+                high = partition - 1;
+            } else {
+                low = partition + 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/test/java/sorting/problems/kth_smallest_element_in_a_array/KthSmallestElementInArrayTest.java
@@ -0,0 +1,48 @@
+package sorting.problems.kth_smallest_element_in_a_array;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class KthSmallestElementInArrayTest {
+
+    @ParameterizedTest
+    @MethodSource("inputs")
+    public void test(int[] input1, int k, int expected) {
+        var kthSmallestElementInArray = new KthSmallestElementInArray();
+
+        int result = kthSmallestElementInArray.kthSmallest(input1, input1.length, k);
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    public static Stream<Arguments> inputs() {
+        return Stream.of(
+                Arguments.of(
+                        new int[]{1, 2, 3, 4},
+                        2,
+                        2
+                ),
+                Arguments.of(
+                        new int[]{7, 10, 4, 3, 20, 15},
+                        3,
+                        7
+                ),
+                Arguments.of(
+                        new int[]{7, 10, 4, 3, 20, 15},
+                        4,
+                        10
+                ),
+                Arguments.of(
+                        new int[]{10, 4, 5, 8, 11, 6, 26},
+                        5,
+                        10
+                )
+        );
+    }
+
+}
