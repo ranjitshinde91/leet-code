56c320188d9c8c51f43d3403df020796884aa779
ranjitshindetwks <98303304+ranjitshindetwks@users.noreply.github.com>
2022-07-21 16:06:15 +0530

    adds LomutoPartition.java

---
+++ src/main/java/sorting/algorithms/quick_sort/partitions/LomutoPartition.java
@@ -0,0 +1,29 @@
+package sorting.algorithms.quick_sort.partitions;
+
+public class LomutoPartition {
+
+
+    public int partition(int[] input, int low, int high, int p) {
+        swap(input, p, high);
+        int i = low - 1;
+        int pivotElement = input[high];
+        for (int j = low; j < high; j++) {
+            if (input[j] <= pivotElement) {
+                i++;
+                swap(input, i, j);
+            }
+        }
+        swap(input, i + 1, high);
+
+        for (int j = low; j <= high; j++) {
+            System.out.print(input[j] +" ");
+        }
+        return i + 1;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
