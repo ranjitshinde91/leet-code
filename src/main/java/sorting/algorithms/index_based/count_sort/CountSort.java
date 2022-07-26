87125aba29f11c3900eb64b7b6ea322bb0549526
ranjitshindetwks <98303304+ranjitshindetwks@users.noreply.github.com>
2022-07-26 12:44:42 +0530

    updates CountSort.java

--- src/main/java/sorting/algorithms/index_based/count_sort/CountSort.java
+++ src/main/java/sorting/algorithms/index_based/count_sort/CountSort.java
@@ -4,8 +4,11 @@ public class CountSort {
 
 
     public void countSort(int[] input, int n) {
+        countSort(input, n, 1);
+    }
+
+    public void countSort(int[] input, int n, int exp) {
         int k = maxOf(input, n);
-        System.out.println(k);
         int[] count = new int[k + 1];
         int[] output = new int[n];
 
@@ -13,21 +16,25 @@ public class CountSort {
             count[i] = 0;
         }
         for (int i = 0; i < n; i++) {
-            int element = input[i];
+            int element = getElement(input[i], exp);
             count[element] = count[element] + 1;
         }
         for (int i = 1; i <= k; i++) {
             count[i] = count[i] + count[i - 1];
         }
         for (int i = n - 1; i >= 0; i--) {
-            int element = input[i];
-            output[count[element] - 1] = element;
+            int element = getElement(input[i], exp);
+            output[count[element] - 1] = input[i];
             count[element] = count[element] - 1;
         }
 
         for (int i = 0; i < n; i++) {
             input[i] = output[i];
         }
+    }
+
+    private int getElement(int input, int exp) {
+        return (input / exp) % 10;
     }
 
     private int maxOf(int[] input, int n) {
