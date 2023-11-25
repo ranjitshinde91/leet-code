09509ca32c10defc1720c1059b8af84a1936b79e
ranjitshinde91 <ranjitshinde91@gmail.com>
2023-11-25 19:10:43 +0530

    adds BinarySearch.java

---
+++ src/main/java/binary_search/BinarySearch.java
@@ -0,0 +1,21 @@
+package binary_search;
+
+// leetcode-704: Binary Search
+public class BinarySearch {
+
+  public int binarySearch(int[] nums, int target) {
+    int low = 0;
+    int high = nums.length - 1;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] > target) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+    return -1;
+  }
+}
---
+++ src/test/java/binary_search/BinarySearchTest.java
@@ -0,0 +1,28 @@
+package binary_search;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class BinarySearchTest {
+
+  @ParameterizedTest
+  @MethodSource("inputs")
+  void test(int[] input, int target, int expected) {
+    var binarySearch = new BinarySearch();
+
+    int result = binarySearch.binarySearch(input, target);
+
+    assertThat(result).isEqualTo(expected);
+  }
+
+  private static Stream<Arguments> inputs() {
+    return Stream.of(
+        Arguments.of(new int[] {-1, 0, 3, 5, 9, 12}, 9, 4),
+        Arguments.of(new int[] {-1, 0, 3, 5, 9, 12}, 2, -1));
+  }
+}
